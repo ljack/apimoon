e453866d80b4bf47442179433a15a22238f4d12b
JarkkoLietolahti <jarkko.lietolahti@gmail.com>
2016-04-07 20:32:04 +0000

    Autocommit

--- apimoon/lib/collections/columns-todo.jsx
+++ apimoon/lib/collections/columns-todo.jsx
@@ -260,29 +260,3 @@ export {
   SaveCollection, DeleteObject
 };
 
-const CustomStyle = {
-  overlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: 'rgba(255, 255, 255, 0.75)'
-  },
-  content: {
-    zIndex: '255',
-    position: 'absolute',
-    top: '60px', // changed to 60px to get below the top menu
-    left: '40px',
-    right: '40px',
-    bottom: '40px',
-    border: '1px solid #ccc',
-    background: '#fff',
-    overflow: 'auto',
-    WebkitOverflowScrolling: 'touch',
-    borderRadius: '4px',
-    outline: 'none',
-    padding: '20px'
-
-  }
-};
