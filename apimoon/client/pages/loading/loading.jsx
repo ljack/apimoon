39e45c4f37c5d37566e4d22110a739ae3ef80696
JarkkoLietolahti <jarkko.lietolahti@gmail.com>
2016-04-09 13:31:33 +0000

    Autocommit

--- apimoon/client/pages/loading/loading.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Loading = () => (
-	<div className="loading">
-		<i className="fa fa-circle-o-notch fa-4x fa-spin"></i>
-	</div>
-);
--- apimoon/client/pages/not_found/not_found.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const NotFound = () => (
-	<div className="container">
-		<h2>Page not found</h2>
-	</div>
-);
