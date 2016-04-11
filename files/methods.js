d32334ba3dbe71be4249a4c63d57f33c2d5d52c1
JarkkoLietolahti <jarkko.lietolahti@gmail.com>
2016-04-11 12:14:22 +0000

    Added favicon.ico, some work done on apiclient codecomponent, tried to implement <ControlledModal> for more general use, needs testing

---
+++ files/methods.js
@@ -0,0 +1,47 @@
+import { Meteor } from 'meteor/meteor';
+import {
+    Apiclients
+}
+from '/lib/collections/apiclients.js';
+
+
+// callRest call HTTP endpoint from server
+export const callRest = new ValidatedMethod({
+    name: 'apiclients.callRest',
+    validate: () => {
+        console.log("validate");
+    },
+    run(
+        rowData
+    ) {
+        this.unblock();
+        console.log("callRest running "); //  rowData=", rowData);
+
+        if (!this.isSimulation) {
+            if( !rowData.userAgent ) {
+                rowData.userAgent = "Apimoon Test App";
+            }
+            var options = {
+              headers: {
+                  "User-Agent": rowData.userAgent
+              }  
+            };
+            var data = HTTP.call(rowData.method, rowData.url,options);
+            let id = rowData._id;
+            let values = {};
+            values.lastResult = JSON.stringify(data);
+
+            console.log("id=" + id + " values=", values);
+            // console.log("Apiclients=", Apiclients);
+            Apiclients.update({
+                _id: id
+            }, {
+                $set: values
+            })
+        }
+
+
+        return "ok";
+    }
+
+});
